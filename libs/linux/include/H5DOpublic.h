ece109d48b7440827a407375fa8414b4949bfb86
anjaldoshi <anjaldoshi09@gmail.com>
2022-02-01 14:08:01 -0800

    Update HDF5 library for Linux

--- libs/linux/include/H5DOpublic.h
+++ libs/linux/include/H5DOpublic.h
@@ -6,13 +6,13 @@
  * This file is part of HDF5.  The full HDF5 copyright notice, including     *
  * terms governing use, modification, and redistribution, is contained in    *
  * the COPYING file, which can be found at the root of the source code       *
- * distribution tree, or in https://support.hdfgroup.org/ftp/HDF5/releases.  *
+ * distribution tree, or in https://www.hdfgroup.org/licenses.               *
  * If you do not have access to either file, you may request a copy from     *
  * help@hdfgroup.org.                                                        *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-#ifndef _H5DOpublic_H
-#define _H5DOpublic_H
+#ifndef H5DOpublic_H
+#define H5DOpublic_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,34 +20,30 @@ extern "C" {
 
 /*-------------------------------------------------------------------------
  *
- * Direct chunk write function
+ * "Optimized dataset" routines.
  *
  *-------------------------------------------------------------------------
  */
 
-H5_HLDLL herr_t H5DOwrite_chunk(hid_t dset_id, 
-			hid_t dxpl_id, 
-			uint32_t filters, 
-			const hsize_t *offset, 
-         		size_t data_size, 
-			const void *buf);
+H5_HLDLL herr_t H5DOappend(hid_t dset_id, hid_t dxpl_id, unsigned axis, size_t extension, hid_t memtype,
+                           const void *buf);
 
-/*-------------------------------------------------------------------------
+/* Symbols defined for compatibility with previous versions of the HDF5 API.
  *
- * Direct chunk read function
- *
- *-------------------------------------------------------------------------
+ * Use of these symbols is deprecated.
  */
+#ifndef H5_NO_DEPRECATED_SYMBOLS
 
-H5_HLDLL herr_t H5DOread_chunk(hid_t dset_id, /*in*/
-            hid_t dxpl_id,         /*in*/
-            const hsize_t *offset, /*in*/
-            uint32_t *filters,     /*out*/
-            void *buf);            /*out*/
+/* Compatibility wrappers for functionality moved to H5D */
+H5_HLDLL herr_t H5DOwrite_chunk(hid_t dset_id, hid_t dxpl_id, uint32_t filters, const hsize_t *offset,
+                                size_t data_size, const void *buf);
+H5_HLDLL herr_t H5DOread_chunk(hid_t dset_id, hid_t dxpl_id, const hsize_t *offset, uint32_t *filters /*out*/,
+                               void *buf /*out*/);
+
+#endif /* H5_NO_DEPRECATED_SYMBOLS */
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif
-
